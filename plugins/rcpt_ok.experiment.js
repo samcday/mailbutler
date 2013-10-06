aa27df1d1ced8df19e97874ac083522e9087f126
Sam Day <sday@atlassian.com>
2013-10-06 17:06:28 +1100

    Early hacks on forwarding emails.

---
+++ plugins/rcpt_ok.experiment.js
@@ -0,0 +1,15 @@
+var Address = require('./address').Address;
+
+var newRcpt = new Address("sam.c.day.throwaway", "gmail.com");
+
+exports.hook_data_post = function(next, connection, params) {
+    connection.relaying = true;
+
+    var txn = connection.transaction;
+    var originalRcpt = txn.rcpt_to[0];
+    txn.rcpt_to[0] = newRcpt;
+    txn.add_header("X-Forwarded-For", newRcpt.toString() + " " + originalRcpt.toString());
+    txn.add_header("X-Forwarded-To", newRcpt.toString());
+
+    next();
+};
